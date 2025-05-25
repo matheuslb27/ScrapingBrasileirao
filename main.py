86b9e2b6bd57784b9ff24d64ed6ac56f62078038
Matheus <matheus.lbarboza@gmail.com>
2025-05-25 20:36:24 -0300

    criação classe calendario

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from scraping.classificacao import Classificacao
 from scraping.estatisticas_times import Estatisticas_times
+from scraping.calendario import Calendario
 
 
 if __name__ == "__main__":
+    
     #Classificacao.salvar_class()
     #Estatisticas_times.salvar_estats()
-    #Classificacao.atualizar_classificacao()
-    Estatisticas_times.atualizar_estats()
+    #Calendario.salvar_rodadas()
 
-    
+    Classificacao.atualizar_classificacao()
+    Estatisticas_times.atualizar_estats()
+    Calendario.atualizar_rodadas()
